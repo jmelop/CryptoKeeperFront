a88c0cf1f4284240016ea35b9485f3f9b166ac42
Juan Melo Pérez <juanmlstm@gmail.com>
2021-07-02 10:46:52 +0100

    New components, home and news

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home/home.component';
+import { LandingComponent } from './components/home/landing/landing.component';
 import { LoginComponent } from './components/home/login/login.component';
 import { RegisterComponent } from './components/home/register/register.component';
 import { CryptosComponent } from './components/user/cryptos/cryptos.component';
+import { HomeComponent } from './components/user/home/home.component';
+import { NewsComponent } from './components/user/news/news.component';
 import { ReportsComponent } from './components/user/reports/reports.component';
 import { KeeperGuard } from './keeper/keeper.guard';
 
@@ -11,11 +13,21 @@ const routes: Routes = [
 
   {
     path: '',
-    component: HomeComponent
+    component: LandingComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [KeeperGuard]
   },
   {
     path: 'cryptos',
     component: CryptosComponent,
+    canActivate: [KeeperGuard]
+  },
+  {
+    path: 'news',
+    component: NewsComponent,
     canActivate: [KeeperGuard]
   },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'; 
 import { FormsModule } from '@angular/forms';
-
+import { HomeComponent } from './components/user/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterComponent } from './components/home/register/register.component';
 import { CryptosComponent } from './components/user/cryptos/cryptos.component';
-import { HomeComponent } from './components/home/home/home.component';
+import { LandingComponent } from './components/home/landing/landing.component';
 import { LoginComponent } from './components/home/login/login.component';
 import { ReportsComponent } from './components/user/reports/reports.component';
 import { ChartsModule } from 'ng2-charts';
 import { CookieService } from 'ngx-cookie-service';
+import { NewsComponent } from './components/user/news/news.component';
 
 
 
@@ -22,9 +23,11 @@ import { CookieService } from 'ngx-cookie-service';
     NavbarComponent,
     RegisterComponent,
     CryptosComponent,
-    HomeComponent,
+    LandingComponent,
     LoginComponent,
-    ReportsComponent
+    HomeComponent,
+    ReportsComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
