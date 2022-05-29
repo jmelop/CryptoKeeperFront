8b65b6389cbc3d4d601f070371370de2b5072d0f
Juan Melo Pérez <juanmlstm@gmail.com>
2022-05-29 19:24:50 +0100

    Added new styles and content in landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       colors: {
         'regal': '#17252A',
-        'regal-dark': '#1F2833'
+        'regal-dark': '#1F2833',
+        'super-indigo': '#7456F1',
       },
       boxShadow: {
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)'
